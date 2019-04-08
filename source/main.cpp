d0abcf201df3ebea13a27347ec1784dfe1ef5b92
Jonathan Young <young.jpc@gmail.com>
2019-04-08 14:09:51 +1000

    Wrap some imported functions for logging.

--- source/main.cpp
+++ source/main.cpp
@@ -4,6 +4,41 @@
 #include <bx/os.h>
 #include "MemoryModule.h"
 
+namespace original {
+
+HMODULE (*LoadLibraryA)(LPCSTR);
+int (WINAPI *MessageBoxA)(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
+
+} // namespace original
+
+namespace wrap {
+
+HMODULE LoadLibraryA(LPCSTR lpLibFileName) {
+	printf("Called LoadLibraryA filename:'%s'\n", lpLibFileName);
+	return original::LoadLibraryA(lpLibFileName);
+}
+
+int WINAPI MessageBoxA(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType)
+{
+	printf("Called MessageBoxA text:'%s' caption:'%s'\n", lpText, lpCaption);
+	return original::MessageBoxA(hWnd, lpText, lpCaption, uType);
+}
+
+} // namespace wrap
+
+struct WrappedFunc
+{
+	char *lib;
+	char *symbol;
+	void *wrap;
+	void **original;
+};
+
+static WrappedFunc s_wrappedFuncs[] = {
+	{ "kernel32.dll", "LoadLibraryA", wrap::LoadLibraryA, (void **)&original::LoadLibraryA },
+	{ "user32.dll", "MessageBoxA", wrap::MessageBoxA, (void **)&original::MessageBoxA }
+};
+
 struct Library
 {
 	char name[256];
@@ -46,9 +81,14 @@ static HCUSTOMMODULE customLoadLibrary(LPCSTR _filename, void * /*_userData*/) {
 }
 
 static FARPROC customGetProcAddress(HCUSTOMMODULE _lib, LPCSTR _proc, void * /*_userData*/) {
+	const char *libName = nullptr;
 	for (const Library &lib : s_libraries) {
-		if (_lib != lib.handle || bx::strCmpI(lib.name, "SMACKW32.DLL") != 0)
-			continue;
+		if (_lib == lib.handle) {
+			libName = lib.name;
+			break;
+		}
+	}
+	if (bx::strCmpI(libName, "SMACKW32.DLL") == 0) {
 		auto ordinal = *((uint32_t *)&_proc);
 		for (const Symbol &symbol : s_smackSymbols) {
 			if (symbol.ordinal == ordinal) {
@@ -56,12 +96,18 @@ static FARPROC customGetProcAddress(HCUSTOMMODULE _lib, LPCSTR _proc, void * /*_
 				break;
 			}
 		}
-		break;
 	}
 	printf("   symbol %s\n", _proc);
 	auto result = (FARPROC)bx::dlsym(_lib, _proc);
 	if (!result)
 		fprintf(stderr, "failed\n");
+	for (const WrappedFunc wrap : s_wrappedFuncs) {
+		if (bx::strCmpI(libName, wrap.lib) == 0 && bx::strCmpI(_proc, wrap.symbol) == 0) {
+			*wrap.original = result;
+			result = (FARPROC)wrap.wrap;
+			break;
+		}
+	}
 	return result;
 }
 
